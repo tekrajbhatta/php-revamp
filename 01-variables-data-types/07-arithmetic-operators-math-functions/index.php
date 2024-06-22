10e87cd08f8214fa8c4dfd71e6c63c4965c8d596
Tek Raj Bhatt <tekbhatt1@gmail.com>
2024-06-23 00:53:15 +0545

    operators and math functions

--- 01-variables-data-types/07-arithmetic-operators-math-functions/index.php
+++ 01-variables-data-types/07-arithmetic-operators-math-functions/index.php
@@ -9,6 +9,59 @@
 | `/`      | Division       |
 | `%`      | Modulus        |
 */
+
+$output = null;
+$num1 = 20;
+$num2 = 10;
+
+// Arithmetic Operators
+$output = "$num1 + $num2 = " . $num1 + $num2;
+$output = "$num1 - $num2 = " . $num1 - $num2;
+$output = "$num1 * $num2 = " . $num1 * $num2;
+$output = "$num1 / $num2 = " . $num1 / $num2;
+$output = "$num1 % $num2 = " . $num1 % $num2;
+
+// Assignment Operators
+$num3 = 10;
+// $num3 = $num3 + 20;
+// $num3 += 20;
+// $num3 -= 20;
+$num3 /= 2;
+
+// rand()
+// $output = rand();
+// $output = getrandmax();
+$output = rand(1, 10);
+
+// round()
+$output = round(4.7);
+
+// ceil()
+$output = ceil(4.2);
+
+// ceil()
+$output = floor(4.7);
+
+// sqrt()
+$output = sqrt(64);
+
+// pi()
+$output = pi();
+
+//abosolute()
+$output = abs(-4.7);
+
+// max()
+// $output = max(1, 2, 3, 4, 5, 40);
+// $output = max([1, 2, 23, 4, 5, 14 ]);
+
+// min()
+$output = min(1, 2, -3, 4, 5, 40);
+$output = min([13, 32, 23, 44, 5, 14 ]);
+
+//number_format()
+$output = number_format(1234567.987654, 3, '.', ',');
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,9 +80,10 @@
     </div>
   </header>
   <div class="container mx-auto p-4 mt-4">
-    <div class="bg-white rounded-lg shadow-md p-6 mt-6">
+    <p class="bg-white rounded-lg shadow-md p-6 mt-6 text-xl">
       <!-- Output -->
-    </div>
+        <?= $output ?>
+    </p>
   </div>
 </body>
 
