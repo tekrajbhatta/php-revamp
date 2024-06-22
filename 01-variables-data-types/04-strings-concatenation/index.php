5b66f34e965c8b6ffa81dea88771e50cdc989b7b
Tek Raj Bhatt <tekbhatt1@gmail.com>
2024-06-22 13:29:57 +0545

    String concatination

--- 01-variables-data-types/04-strings-concatenation/index.php
+++ 01-variables-data-types/04-strings-concatenation/index.php
@@ -1,3 +1,11 @@
+<?php 
+
+$firstName = 'Saggy';
+$lastName = 'Bhatta';
+$fullName = $firstName . ' ' . $lastName;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,7 +25,9 @@
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6">
       <!-- Output -->
-
+      <?php echo 'Hello my name is ' . $fullName . '<br />' . 'New line text' . '<br />'; ?>
+      <?php echo "Hello my name is $fullName <br>"; ?>
+      <?= 'Hello my name is \'Saggy\'' ?>
     </div>
   </div>
 </body>
