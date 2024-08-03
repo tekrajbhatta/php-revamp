ffb574658e2346c80a73a7c9f26ffaffb9b10e55
Tek Raj Bhatt <tekbhatt1@gmail.com>
2024-08-03 21:07:19 +0545

    Pass anonymous fxn as an argument to a fxn, Return anonymous fxn from a fxn

--- 04-functions/08-anonymous-functions-closures/index.php
+++ 04-functions/08-anonymous-functions-closures/index.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('display_errors', 1);
+
 // anonymous function(lambda function) set to a variable
 $sqaure = function ($number) {
     return $number * $number;
@@ -9,11 +11,11 @@ $result = $sqaure(5);
 
 echo "The sqaure of 5 is: " . $result . "<br>";
 
-// Closures
+// Closures ==> Anonymous fxns are called Closures
 function createCounter() {
     $count = 0;
 
-    $counter = function() use (&$count) { // to access the variable outside of the function scope use ampersand and variable name as(&$count)
+    $counter = function() use (&$count) { // to access the variable outside of the function scope use ampersand and variable name as`(&$count)` with the `use` keyword
        return ++$count;
     };
 
@@ -27,3 +29,40 @@ echo $counter() . '<br>';
 echo $counter() . '<br>';
 echo $counter() . '<br>';
 echo $counter() . '<br>';
+
+// Note:- There are two ways to access the global variable inside a fxn
+// 1st way:- pass the global variable as a paramter to the fxn
+// 2nd way:- use the  `use` keyword with ampersand and variable name as`(&$variableName)`
+
+// Passing anonymous function as an argument to a fxn // We can't pass a normal function as a argument
+function display($a_fun) {
+    return $a_fun();
+}
+
+echo display( function() {
+    echo '<br>';
+    return "Passing anonymous function from a fxn";
+});
+
+// Return anonymous function from a fxn // Geekyshows YT channel
+
+function display1() {
+    return function() {
+        echo '<br>';
+        return "Returning anonymous function from a fxn";
+    };
+}
+
+$disp1 = display1();
+echo $disp1();
+
+// Return anonymous function from a fxn with parameter // Geekyshows YT channel
+function display2($str) {
+    return function($abc) use (&$str) {
+        echo '<br>';
+        return "$str $abc Returning anonymous function from a fxn with parameter";
+    };
+}
+
+$disp2 = display2("Hello ");
+echo $disp2('ABC, ');
